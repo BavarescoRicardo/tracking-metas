13568d3373a46162d9d35a8615540ddd673eda2f
Bavaresco Ricardo <bavaresco.ricardo@gmail.com>
2023-10-16 23:45:22 -0300

    ajustes layout lateral

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,37 @@
+<template>
+    <header>
+        <h1>
+            <img src="../assets/objetivo.png" alt="Seus Objetivos">
+        </h1>
+    </header>
+</template>
+<script lang="ts">
+import { DefineComponent } from 'vue';
+
+export default {
+    name: 'BarraLateral'
+}
+</script>
+<style scoped>
+header {
+  background: #0a0a18;
+  width: 100%;
+  height: 100vh;
+}
+
+img {
+  display: flex;
+  height: auto;
+  max-width: 25%;
+  padding: 1rem 0;
+  margin-left: 5rem;
+  border-radius: 3rem;
+}
+
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+</style>
